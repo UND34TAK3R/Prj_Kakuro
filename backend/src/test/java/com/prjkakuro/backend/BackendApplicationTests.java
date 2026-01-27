1dccc3b9fd44fbefb9b4c93acb070068a74c4bb1
Derrick Mangari <dmangari@live.fr>
2026-01-26 22:12:49 -0500

    Project Setup and first commit

---
+++ backend/src/test/java/com/prjkakuro/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prjkakuro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
